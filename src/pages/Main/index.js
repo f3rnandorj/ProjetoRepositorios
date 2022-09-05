83265f0b8f036789cce3d089661a60764eb58a7b
FERNANDO HENRIQUE FERNANDES DA SILVA <f3rnandorj10@gmail.com>
2022-09-05 19:25:44 -0300

    GlobalStyle add
    Adicionando Global Style

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -3,6 +3,14 @@ import React from 'react';
 import { Title } from './styles';
 
 function Main() {
+  return <Title>Hello world!</Title>;
+}
+
+export default Main;
+
+/* COMO SERIA UM ENCADEAMENTO
+
+function Main() {
   return (
     <Title error={false}>
       Main
@@ -10,5 +18,4 @@ function Main() {
     </Title>
   );
 }
-
-export default Main;
+  }*/
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,6 +1,12 @@
 import styled from 'styled-components';
 
 export const Title = styled.h1`
+  color: #fff;
+`;
+
+/* COMO SERIA UM ENCADEAMENTO
+
+  export const Title = styled.h1`
   font-size: 24px;
   color: ${(props) => (props.error ? 'red' : '#7159c1')};
   font-family: Arial, Helvetica, sans-serif;
@@ -8,5 +14,4 @@ export const Title = styled.h1`
   small {
     font-size: 10px;
     color: #333;
-  }
-`;
+  }*/
