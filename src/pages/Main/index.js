206e9bca8625110ed78be70a54c806ffca099dae
FERNANDO HENRIQUE FERNANDES DA SILVA <f3rnandorj10@gmail.com>
2022-09-11 13:54:27 -0300

    Using localStorage
    salvando e carregando dados armazenados no localStorage

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -12,6 +12,24 @@ export default class Main extends Component {
     loading: false,
   };
 
+  //Carregar os dados do localstorage
+  componentDidMount() {
+    const repositories = localStorage.getItem('repositories');
+
+    if (repositories) {
+      this.setState({ repositories: JSON.parse(repositories) });
+    }
+  }
+
+  //Salvar os dados do localstorage
+  componentDidUpdate(_, prevState) {
+    const { repositories } = this.state;
+
+    if (prevState.repositories !== repositories) {
+      localStorage.setItem('repositories', JSON.stringify(repositories));
+    }
+  }
+
   //pegando o novo valor e adicionando no state newRepo
   handleInputChange = (e) => {
     this.setState({ newRepo: e.target.value });
